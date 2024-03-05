7a909b476dcb6183b818edb32d93854fcdf275a1
Damian Murawski <dmurawski018@gmail.com>
2024-03-05 17:07:41 +0100

    4_AdminModele_i_Relacje

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Film, ExtraInfo, Ocena, Aktor
 
-# Register your models here.
+
+admin.site.register(Aktor)
+
+class ExtraInfoInline(admin.TabularInline):
+    model = ExtraInfo
+
+class OcenaInline(admin.TabularInline):
+    model = Ocena
+    extra = 0
+
+class AktorInline(admin.TabularInline):
+    model = Aktor.filmy.through
+    extra = 0
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    inlines = [ExtraInfoInline, OcenaInline, AktorInline]
--- filmy/models.py
+++ filmy/models.py
@@ -9,4 +9,50 @@ class Film(models.Model):
     imdb_pkts = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
-       return "{} ({})".format(self.tytul, str(self.rok))
+        return "{} ({})".format(self.tytul, str(self.rok))
+
+
+class ExtraInfo(models.Model):
+    GATUNEK = {
+        (0, 'Inne'),
+        (1, 'Horror'),
+        (2, 'Komedia'),
+        (3, 'Sci-fi'),
+        (4, 'Dramat')
+    }
+    czas_trwania = models.PositiveSmallIntegerField(null=True, blank=True)
+    gatunek = models.PositiveSmallIntegerField(choices=GATUNEK, null=True, blank=True)
+    punkty_widzow = models.PositiveSmallIntegerField(default=0)
+    film = models.OneToOneField(Film, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        for g in list(self.GATUNEK):
+            if g[0] == self.gatunek:
+                gok = g[1]
+        return "Film: {}, gatunek: {}, czas trwania: {}, punkty od widzów: {}".format(self.film.tytul,
+                                                                                      gok, self.czas_trwania,
+                                                                                self.punkty_widzow)
+class Ocena(models.Model):
+    recenzja = models.TextField(default="", blank=True)
+    gwiazdki = models.PositiveSmallIntegerField(default=5)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE,null=True, blank=True)
+
+    # <-- relacja many-to-1 z modelem 'Film'
+    # Po ustaleniu relacji z modelem Film możliwa jest np. następująca
+    # zmiana reprezentacji tekstowej modelu Ocena
+
+    def __str__(self):
+        rec = self.recenzja[:20] + ' ...'
+        return "Film: {}, gwiazdki: {}, recenzja: {}".format(self.film.tytul, str(self.gwiazdki), rec)
+
+class Aktor(models.Model):
+    imie = models.CharField(max_length=32)
+    nazwisko = models.CharField(max_length=32)
+    filmy = models.ManyToManyField(Film)
+
+    # <-- relacja many-to-many z modelem 'Film'
+    # Po ustaleniu relacji z modelem Film możliwa jest np.
+    # następująca zmiana reprezentacji tekstowej modelu Aktor
+
+    def __str__(self):
+        return "{} {}, gra w {} filmach z bazy danych".format(self.imie, self.nazwisko, str(self.filmy.count()))
